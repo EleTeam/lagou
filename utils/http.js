2a692baf5b3acb583f8e0babf1207ab0ac8adbe2
Mr. Guo <474224024@qq.com>
2017-09-15 23:22:43 +0800

    简单的微信网络请求promise化

---
+++ utils/http.js
@@ -0,0 +1,41 @@
+/*
+ * @Author: guojingfeng
+ * @Date: 2017-09-15 13:37:55
+ * @Last Modified by: guojingfeng
+ * @Last Modified time: 2017-09-15 23:22:38
+ */
+var baseUrl = 'http://guojingfeng.nat123.net/api/'
+var api = {
+  indexList: 'index/list'
+}
+
+/**
+ * 对微信网络请求的简易封装
+ *
+ * @param {any} apiName 接口地址对应名称
+ * @param {any} options 微信请求支持的参数
+ * @returns
+ */
+module.exports = function (apiName, options, callback) {
+  return new Promise((resolve, reject) => {
+    var config = {
+      url: baseUrl + api[apiName],
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      // header: {}, // 设置请求的 header
+      success: function (res) {
+        // success
+        resolve(res)
+      },
+      fail: function () {
+        // fail
+        reject('fail')
+      }
+    }
+
+    if (options) Object.assign(config, options)
+
+    var requestTask = wx.request(config)
+
+    callback && callback(requestTask)
+  }).catch(err => console)
+}
