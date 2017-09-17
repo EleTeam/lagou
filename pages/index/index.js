e4a478397b13d4a9b8ee398a32e597849fa90d4b
Mr. Guo <474224024@qq.com>
2017-09-17 17:13:43 +0800

    完成首页数据加载的完整流程

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,10 +5,13 @@ const app = getApp()
 
 Page({
   data: {
+    pageNo: 1,
+    showBtn: false, // 默认不显示加载更多按钮
+    isLoading: false, // 请求状态
     positionList: []
   },
   //事件处理函数
-  gotoCustomInfo: function() {
+  gotoCustomInfo: function () {
     wx.navigateTo({
       url: '../customInfo/customInfo'
     })
@@ -22,14 +25,46 @@ Page({
       url: `../positionDetail/positionDetail?positionId=${e.currentTarget.dataset.pid}`,
     })
   },
-  onLoad: function () {
-    http(app.apiName.indexList).then(res => {
+
+  /**
+   * 加载更多
+   *
+   */
+  loadMore() {
+    this.loadData(this.data.pageNo + 1)
+  },
+  /**
+   * 通用请求数据函数
+   *
+   * @param {Number} pageNo 自定义的页码请求
+   */
+  loadData(pageNo) {
+    var options = {}
+    if (pageNo) options.data = { pageNo: pageNo }
+    this.setData({ isLoading: true })
+    http(app.apiName.indexList, options).then(res => {
+      this.setData({ isLoading: false })
       for (let item of res.data.content.data.page.result) {
         item.companyLogo = app.picHost + item.companyLogo
       }
-      this.setData({
-        positionList: res.data.content.data.page.result
-      })
+      if (parseInt(res.data.content.data.page.start) + res.data.content.data.page.pageSize < res.data.content.data.page.totalCount) {
+        this.setData({ showBtn: true })
+      } else {
+        this.setData({ showBtn: false })
+      }
+      this.setData({ pageNo: res.data.content.data.page.pageNo })
+      if (pageNo) {
+        this.setData({
+          positionList: this.data.positionList.concat(res.data.content.data.page.result)
+        })
+      } else {
+        this.setData({
+          positionList: res.data.content.data.page.result
+        })
+      }
     })
+  },
+  onLoad: function () {
+    this.loadData()
   }
 })
